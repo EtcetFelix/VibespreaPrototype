227ea7137e55d26586c78ed7b231b17e7a158eff
Alan Bohannon <alanbohannon@gmail.com>
2022-12-03 08:34:34 -0800

    Initial Commit

---
+++ VibespreaPrototype/VibespreaPrototypeApp.swift
@@ -0,0 +1,20 @@
+//
+//  VibespreaPrototypeApp.swift
+//  VibespreaPrototype
+//
+//  Created by Alan Bohannon on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct VibespreaPrototypeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
